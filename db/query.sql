1bbd6727374e9cdc6583693245c633a5f4dc23a2
Phinjock Sherpa <phinjock@gmail.com>
2022-09-06 22:01:30 -0700

    Manager name now appears on view all employees

--- db/query.sql
+++ db/query.sql
@@ -1,8 +1,11 @@
-SELECT employee.first_name, employee.last_name, roles.title, department.department_name, roles.salary, employee.manager_id
-FROM department
-INNER JOIN roles
-ON department.id=roles.department_id
-INNER JOIN employee
-ON roles.id=employee.role_id
+SELECT employee.first_name, employee.last_name, roles.title, department.department_name, roles.salary, employee.manager_id,
+CONCAT(manager.first_name,' ',manager.last_name) AS manager
+FROM employee
+LEFT JOIN roles
+ON employee.role_id=roles.id
+LEFT JOIN department
+ON roles.department_id=department.id
+LEFT JOIN employee manager 
+ON manager.id =employee.manager_id
 ORDER BY department.id;
 
